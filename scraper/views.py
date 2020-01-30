6ec7df1188fdc859339557ea2970f8714d69ce16
LoopThrough-i-j <dganguly1120@gmail.com>
2020-01-30 21:47:50 +0530

    commit

--- scraper/views.py
+++ scraper/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from requests.compat import quote_plus
 from . import models
 
-BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+BASE_CRAIGSLIST_URL = 'https://kolkata.craigslist.org/search/?query={}'
 BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 
